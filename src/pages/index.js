33c2ae7fe828e4112b56be430354710e7a8072fa
Zach Donsky <zach@chalmerscards.com>
2019-09-12 21:44:39 -0400

    removed email form

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,31 +82,13 @@ class HomeIndex extends React.Component {
                         <br /><br />
                         <h3>Get In Touch</h3>
                         If you would like to volunteer, partner, or donate, please contact us here.</p>
-                        <div className="row">
-                            <div className="8u 12u$(small)">
-                                <form method="post" action="#">
-                                    <div className="row uniform 50%">
-                                        <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
-                                        <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
-                                        <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
-                                    </div>
-                                </form>
-                                <ul className="actions">
-                                    <li><input type="submit" value="Send Message" /></li>
-                                </ul>
-                            </div>
-                            <div className="4u 12u$(small)">
-                                <ul className="labeled-icons">
-                                    <li>
-                                        <h3 className="icon fa-home"><span className="label">Address</span></h3>
-                                        <a href="https://civictech.ca">Civic Tech TO</a>
-                                    </li>
-                                    <li>
-                                        <h3 className="icon fa-envelope-o"><span className="label">Email</span></h3>
-                                        <a href="mailto:hello@acceleratorproject.xyz">hello@acceleratorproject.xyz</a>
-                                    </li>
-                                </ul>
-                            </div>
+                          <div className="4u 12u$(small)">
+                            <ul className="labeled-icons">
+                                <li>
+                                    <h3 className="icon fa-envelope-o"><span className="label">Email</span></h3>
+                                    <a href="mailto:hello@acceleratorproject.xyz">hello@acceleratorproject.xyz</a>
+                                </li>
+                            </ul>
                         </div>
                     </section>
                 </div>
